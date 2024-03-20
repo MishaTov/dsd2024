4a222f26e47d8e67101cf36834937101fe48418d
MishaTov <106400397+MishaTov@users.noreply.github.com>
2024-03-21 01:27:17 +0200

    laba2

--- laba2/dist_map.py
+++ laba2/dist_map.py
@@ -9,7 +9,7 @@ config.cluster_members = ['127.0.0.1:5701', '127.0.0.1:5702', '127.0.0.1:5703']
 config.client_name = 'MishaPy'
 client = HazelcastClient(config=config)
 map_ = client.get_map('map')
-# for key in range(1000):
-#     map_.put(str(key), randint(0, 1000))
+for key in range(1000):
+    map_.put(str(key), randint(0, 1000))
 client.shutdown()
 
